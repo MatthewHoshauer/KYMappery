b3929f336dfaac548d56e41ca6aeabdcf94d3a31
MatthewHoshauer <matthewhoshauer@gmail.com>
2023-09-19 05:46:19 -0500

    added html and js / geojson files

---
+++ kentucky_map.js
@@ -0,0 +1,17 @@
+// Initialize the map centered around Kentucky
+var map = L.map('map').setView([37.5, -85], 7); // Adjust the initial coordinates and zoom level
+
+// Add a tile layer to the map (You can choose different tile providers)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Load the GeoJSON data for Kentucky's boundaries (Replace 'kentucky.geojson' with your file path)
+fetch('kentucky.geojson') // IMPORTANT: Adjust the path to your GeoJSON file
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // Create a GeoJSON layer and add it to the map
+        L.geoJSON(data).addTo(map);
+    });
